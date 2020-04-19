fa58e4fe051cff4f110e36642ec8c897ea66c5b5
Henry Chang <henry5988@hotmail.com>
2020-04-19 18:11:40 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="app container">
+      <div class="row justify-content-center">
+        <div class="col">
+        <question :text="q_title" :a="q_answer[0]" :b="q_answer[1]" :c="q_answer[2]" :d="q_answer[3]" :feedback="q_feedback"></question>
+        </div>
+      </div>
+        <div class="row justify-content-center">
+            <div class="col-5 btn btn-primary" @click="nextQuestion">Next Question</div>
+            <div class="offset-1 col btn btn-warning" v-if="q_n!=1">Last Question</div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import json from '../public/export_dataframe.json';
+    import question from "./components/question";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+    export default {
+        name: 'App',
+        components: {
+            question
+        },
+        methods: {
+            onValidate(results) {
+                this.results = results;
+            },
+            nextQuestion(){
+                this.q_n++;
+            }
+        },
+        data() {
+            let answers = [];
+            let ids = [];
+            let isCorrects = [];
+            let titles = null;
+            let feedback = null;
+            let n_questions = 1;
+            for (let key in json.Question_id) {
+                console.log(key);
+                console.log(`value of ${key} right now: ${json.Question_id[key]}`);
+                if (json.Question_id[key] == n_questions) {
+                    ids.push(key);
+                    console.log("ids right now: " + ids);
+                }
+            }
+            for (let key in json.Answer_text) {
+                if (ids.includes(key))
+                    answers.push(json.Answer_text[key]);
+            }
+            for (let key in json.is_correct) {
+                if (ids.includes(key))
+                    isCorrects.push(json.is_correct[key]);
+            }
+            for (let key in json.Question_text) {
+                if (ids.includes(key))
+                    titles = json.Question_text[key];
+            }
+            for(let key in json.feedback_text){
+              if(ids.includes(key))
+                feedback = json.feedback_text[key];
+            }
+
+            return {
+                df: json,
+                currentQ: n_questions,
+                q_answer: answers,
+                q_id: ids,
+                q_isCorrect: isCorrects,
+                q_title: titles,
+                q_n: n_questions,
+                q_feedback: feedback,
+            };
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    @import'~bootstrap/dist/css/bootstrap.css';
 </style>
---
+++ src/components/question.vue
@@ -0,0 +1,94 @@
+<template>
+    <label class="question">
+        <div class="form-group my-4">
+            <div class="row justify-content-center my-1">
+                <div class="col-10">
+                    <strong>{{title}}</strong>
+                </div>
+            </div>
+            <div class="row justify-content-center my-1 mb-3">
+                <div class=" col-10">
+                    {{text}}
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" :name="title" :id="title + a" value="optionA">
+                        <label class="form-check-label" :for="title + a">
+                            A. {{a}}
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" :name="title" :id="title + b" value="optionB">
+                        <label class="form-check-label" :for="title + b">
+                            B. {{b}}
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" :name="title" :id="title + c" value="optionC">
+                        <label class="form-check-label" :for="title + c">
+                            C. {{c}}
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" :name="title" :id="title + d" value="optionD">
+                        <label class="form-check-label" :for="title + d">
+                            D. {{d}}
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </label>
+</template>
+
+<script>
+
+    export default {
+        name: "question",
+        props:{
+
+            text: String,
+            a: String,
+            b: String,
+            c: String,
+            d: String,
+            feedback: String,
+        },
+        data: function () {
+            return {
+                isTesting: true,
+                titleNum: 1,
+            }
+        },
+        computed:{
+            title: function(){
+                return("Question "+ this.titleNum);
+            },
+        },
+            methods: {
+                submitQuiz: function () {
+                    this.isTesting = false;
+                }
+            }
+        }
+
+
+</script>
+
+<style scoped>
+
+</style>
